83ceaf5726a0098e0c5e2a4664175838c8dd2c7a
kanlee <kanlee@student.42seoul.kr>
2021-04-22 01:08:30 +0900

    checker done

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,13 +6,14 @@
 /*   By: kanlee <kanlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 22:57:21 by kanlee            #+#    #+#             */
-/*   Updated: 2021/04/21 17:54:35 by kanlee           ###   ########.fr       */
+/*   Updated: 2021/04/22 00:12:11 by kanlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# define STDIN 0
 # define STDOUT 1
 # define STDERR 2
 
@@ -31,7 +32,6 @@ typedef struct	s_stack {
 	int			max;
 }				t_stack;
 
-
 void	free_split(char **str);
 void	free_stacks(t_stack *a, t_stack *b);
 int		fill_stack_a(t_stack *a, char **av);
@@ -39,6 +39,22 @@ int		fill_stack_a(t_stack *a, char **av);
 void	stack_add_top(t_stack *stack, int n);
 void	stack_del_top(t_stack *stack);
 void	stack_add_end(t_stack *stack, int n);
+
+
+typedef enum	e_ops {
+	ERR = -1,
+	DO_SA,
+	DO_SB,
+	DO_SS,
+	DO_PA,
+	DO_PB,
+	DO_RA,
+	DO_RB,
+	DO_RR,
+	DO_RRA,
+	DO_RRB,
+	DO_RRR
+}				t_ops;
 
 void	op_sa(t_stack *a);
 void	op_sb(t_stack *b);
--- srcs/checker/main.c
+++ srcs/checker/main.c
@@ -6,7 +6,7 @@
 /*   By: kanlee <kanlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 23:43:20 by kanlee            #+#    #+#             */
-/*   Updated: 2021/04/21 22:18:44 by kanlee           ###   ########.fr       */
+/*   Updated: 2021/04/22 00:56:55 by kanlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include "libft.h"
 #include "push_swap.h"
+#include "get_next_line.h"
 
 static int error(t_stack *a, t_stack *b)
 {
@@ -38,23 +39,102 @@ void prn_stack(t_stack st)
 	}
 }
 
+int chk_op(char *line)
+{
+	if (ft_strequ(line, "sa") == 1)
+		return (DO_SA);
+	if (ft_strequ(line, "sb") == 1)
+		return (DO_SB);
+	if (ft_strequ(line, "ss") == 1)
+		return (DO_SS);
+	if (ft_strequ(line, "pa") == 1)
+		return (DO_PA);
+	if (ft_strequ(line, "pb") == 1)
+		return (DO_PB);
+	if (ft_strequ(line, "ra") == 1)
+		return (DO_RA);
+	if (ft_strequ(line, "rb") == 1)
+		return (DO_RB);
+	if (ft_strequ(line, "rr") == 1)
+		return (DO_RR);
+	if (ft_strequ(line, "rra") == 1)
+		return (DO_RRA);
+	if (ft_strequ(line, "rrb") == 1)
+		return (DO_RRB);
+	if (ft_strequ(line, "rrr") == 1)
+		return (DO_RRR);
+	return (ERR);
+}
+
+void	do_op(t_stack *a, t_stack *b, int opnum)
+{
+	if (opnum == DO_SA)
+		return (op_sa(a));
+	if (opnum == DO_SB)
+		return (op_sb(b));
+	if (opnum == DO_SS)
+		return (op_ss(a, b));
+	if (opnum == DO_PA)
+		return (op_pa(a, b));
+	if (opnum == DO_PB)
+		return (op_pb(a, b));
+	if (opnum == DO_RA)
+		return (op_ra(a));
+	if (opnum == DO_RB)
+		return (op_rb(b));
+	if (opnum == DO_RR)
+		return (op_rr(a, b));
+	if (opnum == DO_RRA)
+		return (op_rra(a));
+	if (opnum == DO_RRB)
+		return (op_rrb(b));
+	if (opnum == DO_RRR)
+		return (op_rrr(a, b));
+}
+
+int	is_sorted(t_stack a, t_stack b)
+{
+	int		n;
+	t_item	*tmp;
+
+	if (b.head != NULL)
+		return (0);
+	tmp = a.head;
+	n = tmp->num;
+	while (tmp->next != a.head)
+	{
+		if (n > tmp->next->num)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
 int main(int ac, char **av) {
 	t_stack	a;
 	t_stack	b;
+	char	*line;
+	int	opnum;
 
-	printf("checker\n");
 	ft_memset(&a, 0, sizeof(t_stack));
 	ft_memset(&b, 0, sizeof(t_stack));
-
 	if (ac < 2)
 		return (error(&a, &b));
 	if (fill_stack_a(&a, av) == FAIL)
 		return (error(&a, &b));
-	
-
-
-
-	prn_stack(a);
+	while (get_next_line(STDIN, &line) > 0)
+	{
+		opnum = chk_op(line);
+		free(line);
+		if (opnum == ERR)
+			return (error(&a, &b));
+		do_op(&a, &b, opnum);
+	}
+	free(line);
+	if (is_sorted(a, b))
+		ft_putstr_fd("OK\n", STDOUT);
+	else
+		ft_putstr_fd("KO\n", STDOUT);
 	free_stacks(&a, &b);
-	return 0;
+	return (0);
 }
