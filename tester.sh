267834bd06a7ef9f4141e24acdefa0bd2fbb49c4
kanlee <kanlee@student.42seoul.kr>
2021-04-27 15:26:42 +0900

    fix tester script in bash

--- tester.sh
+++ tester.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/zsh
+#!/bin/bash
 
 bar="[....................................................................................................]"
 function put_line() {
 	echo -en "\r$bar\r["
-	for i in {1..$i}; do
+	loopend=$1
+	for ((i=1; i<=loopend; i++)); do
 		echo -en "\033[0;32m#\033[0;0m"
 	done
 }
 
 function test_avg() {
-	echo -e 'Testing 100 times in a range from' $1 'to' $2
-	put_line
+	echo -e 'Testing 100 times in range from' $1 'to' $2
 	ERR=0
 	let sum=0
 	for i in {1..100}; do
@@ -21,7 +21,7 @@ function test_avg() {
 			break
 		else
 			put_line $i
-			RET=`./push_swap $ARG | wc -l`
+			RET=`./push_swap $ARG | wc -l | xargs`
 			sum=`expr $sum + $RET` 
 		fi
 	done
@@ -37,11 +37,11 @@ function test_avg() {
 
 
 if [ "$1" = "" ]; then
-	test_avg 0 49
 #	test_avg 0 99
-#	test_avg 0 499
+	test_avg 0 499
 else
 	ARG=`ruby -e "puts (1..$1).to_a.shuffle.join(' ')"`
 	./push_swap $ARG
+	echo -e "Test in range from 1 to" $1
 	./push_swap $ARG | ./checker $ARG
 fi
